2d4d37b1ffda03d6249e54f0ee7446b25c422d67
nxy98 <nxy98@outlook.com>
2025-03-10 17:09:51 -0400

    variable updated

--- modules/blob_storage/variables.tf
+++ modules/blob_storage/variables.tf
@@ -7,20 +7,6 @@ variable "location" {
   default = "East US"
 }
 
-variable "container_name" {
-  type = string
-}
-
-variable "blob_size_mb" {
-  type    = number
-  default = 4096 # 4 GB
-}
-
-variable "container_access_type" {
-  type    = string
-  default = "private"
-}
-
 variable "storage_account_name" {
   type = string
 }
