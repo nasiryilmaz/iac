3dc04c1efacfb2e66388425b95bf467be2348e5a
nxy98 <nxy98@outlook.com>
2025-03-10 15:51:01 -0400

    main.tf created

---
+++ modules/blob_storage/main.tf
@@ -0,0 +1,26 @@
+resource "azurerm_resource_group" "storage_rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.storage_rg.name
+  location                 = azurerm_resource_group.storage_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "storage_container" {
+  name                  = var.container_name
+  storage_account_name  = azurerm_storage_account.storage_account.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "blob" {
+  name                   = var.blob_name
+  storage_account_name   = azurerm_storage_account.storage_account.name
+  storage_container_name = azurerm_storage_container.storage_container.name
+  type                   = "Page"
+  size                   = var.blob_size_mb
+}
