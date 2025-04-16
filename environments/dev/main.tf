20444a796a78dcb35a718152bf7281352bc3593b
nxy98 <nxy98@outlook.com>
2025-04-16 00:59:27 -0400

    main.tf updated

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,11 +1,10 @@
 module "blob_storage" {
-  source = "../modules/blob_storage"
-
-  resource_group_name     = "rg-iac-dev"
-  location                = "eastus"
-  storage_account_name    = "iacstorageacctdev"
-  account_tier            = "Standard"
-  replication_type        = "LRS"
-  container_name          = "container_a"
-  container_access_type   = "private"
+  source                 = "../../modules/blob_storage"
+  resource_group_name    = "rg-iac-dev-east"
+  location               = "East US"
+  storage_account_name   = "iacdevstorage01"
+  account_tier           = "Standard"
+  replication_type       = "LRS"
+  container_name         = "iac-container"
+  container_access_type  = "private"
 }
